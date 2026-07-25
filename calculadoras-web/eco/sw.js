39a2d78ceefa48eb007dcca52bfd278101ae85b6
46372862 <150103239+46372862@users.noreply.github.com>
2026-07-25 11:14:47 -0500

    Add files via upload

---
+++ calculadoras-web/eco/sw.js
@@ -0,0 +1,4 @@
+const CACHE='eco-v1';
+self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(['./','./index.html'])));self.skipWaiting();});
+self.addEventListener('activate',e=>{e.waitUntil(self.clients.claim());});
+self.addEventListener('fetch',e=>{if(e.request.method!=='GET')return;e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request).then(resp=>{const cp=resp.clone();caches.open(CACHE).then(c=>c.put(e.request,cp));return resp;}).catch(()=>caches.match('./index.html'))));});
