5241112aab10ad3c2ffca872e5384f0c0fb93bd2
46372862 <150103239+46372862@users.noreply.github.com>
2026-07-27 20:46:50 -0500

    Add files via upload

--- calculadoras-web/sw.js
+++ calculadoras-web/sw.js
@@ -1,4 +1,4 @@
-const CACHE='hub-202607271937';
+const CACHE='hub-202607272036';
 self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(['./','./index.html'])));self.skipWaiting();});
 /* borra las cachés viejas: sin esto el celular seguiría mostrando la versión anterior */
 self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(ks=>Promise.all(ks.map(k=>k===CACHE?null:caches.delete(k)))).then(()=>self.clients.claim()));});
